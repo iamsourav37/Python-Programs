08a8882f9623c9edc4285d41066b732c27761403
iamSourav37 <souravsrk120@gmail.com>
2020-02-10 19:34:33 +0530

    twisted prime number , some bugs there in reverse_number function

---
+++ twisted_prime_number.py
@@ -0,0 +1,38 @@
+# Program to determine whether a given number is a twisted prime number
+
+'''
+A number is called a twisted prime number if it is a prime number and reverse( 13 ->31 , 17 -> 71 ) of this number is also a prime number.
+
+Examples: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79
+'''
+
+
+def is_prime(number):
+    flag = True
+    for i in range(2,number//2+1):
+        if number % i == 0:
+            flag = False
+            break
+    return flag
+
+
+def reverse_number(number):
+    reverse = 0
+    while number != 0:
+        reverse = reverse + (number%10)*10
+        number //= 10
+    print('reverse in fun',reverse)
+    return reverse
+
+number = int(input("Enter any number : "))
+
+if is_prime(number):
+    reverse = reverse_number(number)
+    print(reverse)
+    if is_prime(reverse):
+        print(number,"is a twisted prime number")
+    else:
+        print(number,"is not a twisted prime number")
+else:
+    print(number, "is not a prime number")
+
