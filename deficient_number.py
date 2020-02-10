5e73d428ccc7bcd943f2717f2492706434dae74f
iamSourav37 <souravsrk120@gmail.com>
2020-02-10 19:01:37 +0530

    deficient number

---
+++ deficient_number.py
@@ -0,0 +1,26 @@
+# Program to determine whether a given number is a Deficient number
+'''
+The deficient number can be defined as the number for which the sum of the proper divisors is lesser than the number itself.
+For example, the number 21 with its proper divisors (1, 3 and 7) has sum (11) lesser than itself.
+'''
+
+
+def sum_of_divisor(number):
+    sum = 0
+
+    for i in range(1,number//2+1):
+        if number % i == 0:
+            sum += i
+    return sum
+
+number = int(input("Enter any number :"))
+sum = sum_of_divisor(number)
+print(sum)
+if sum<number:
+    print(number,"is a Deficient number")
+else:
+    print(number,"is not a Deficient number")
+
+
+
+
