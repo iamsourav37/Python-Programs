0e02bc3693c1697f9d473b99a3878808853bf300
iamSourav37 <souravsrk120@gmail.com>
2020-02-11 11:18:00 +0530

    logic defined for frequency of an element in array

--- array_programs/frequency_of_an_element.py
+++ array_programs/frequency_of_an_element.py
@@ -1,6 +1,28 @@
 # Program to find the frequency of each element of an array
 
-my_array = [12,23,21,12,23,4,5,6,7,5,4]
+
+def frequency_counter(value,full_array):
+    count = 0
+    for i in full_array:
+        if value == i:
+            count += 1
+    return count
+
+arr = [1, 2, 8, 3, 2, 2, 2, 5, 1]
+
+for i in range(len(arr)):
+    print(f"Frequency of {arr[i]} is {frequency_counter(arr[i],arr)}")
 
 
-# abhi toh bohot kuch baaki hai
+
+
+
+
+
+
+
+
+
+
+
+
