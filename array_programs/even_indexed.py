366f47ff428e49c00fa4c05885281fa772d06fe3
iamSourav37 <souravsrk120@gmail.com>
2020-02-11 20:01:18 +0530

    even indexed elements

---
+++ array_programs/even_indexed.py
@@ -0,0 +1,21 @@
+#  Program to print the elements of an array present on even position
+
+arr = [12,23,34,57,43,323,4,7,8,9,11,37,120,121,123,333,1]
+
+print("Printing the elements present on even position : ")
+
+for i in range(len(arr)):
+    if i%2==0:
+        print(f'arr[{i}] = {arr[i]}')
+
+
+
+
+
+
+
+
+
+
+
+
