8fd2341bbb3438af8eb220b62c98f4686cbb3432
iamSourav37 <souravsrk120@gmail.com>
2020-02-11 20:19:20 +0530

    sum of all array elements

---
+++ array_programs/sum_of_all_elements.py
@@ -0,0 +1,10 @@
+# Program to print the sum of all the elements of an array
+
+arr = [1,2,3,4,5]
+
+sum = 0
+for i in range(len(arr)):
+    sum += arr[i]
+print("Sum of all elements of the array",arr," is :",sum)
+
+
