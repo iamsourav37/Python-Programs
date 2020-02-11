2097290154c7b9d701865ed68fc93901446085be
iamSourav37 <souravsrk120@gmail.com>
2020-02-11 20:05:11 +0530

    method 2 defined

--- array_programs/even_indexed.py
+++ array_programs/even_indexed.py
@@ -3,10 +3,16 @@
 arr = [12,23,34,57,43,323,4,7,8,9,11,37,120,121,123,333,1]
 
 print("Printing the elements present on even position : ")
-
+print('"Method 1"')
 for i in range(len(arr)):
     if i%2==0:
         print(f'arr[{i}] = {arr[i]}')
+
+
+print('"Method 2"')
+for i in range(0,len(arr),2):
+    print(f'arr[{i}] = {arr[i]}')
+
 
 
 
