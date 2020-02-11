aff9d4a7ab92b08c675398ae2d9cd2152b6065a0
iamSourav37 <souravsrk120@gmail.com>
2020-02-11 11:26:24 +0530

    better logic

--- array_programs/frequency_of_an_element.py
+++ array_programs/frequency_of_an_element.py
@@ -8,10 +8,12 @@ def frequency_counter(value,full_array):
             count += 1
     return count
 
-arr = [1, 2, 8, 3, 2, 2, 2, 5, 1]
 
-for i in range(len(arr)):
-    print(f"Frequency of {arr[i]} is {frequency_counter(arr[i],arr)}")
+arr = [1, 2, 8, 3, 2, 2, 2, 5, 1]
+unique_values = set(arr)
+
+for i in unique_values:
+    print(f"Frequency of {i} is {frequency_counter(i,arr)}")
 
 
 
